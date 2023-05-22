e3d1df2da970110a6e742a1e91ac331654f70de0
rragul <r.ragul0598@gmail.com>
2023-05-22 09:57:05 +0530

    Update with hasRole authentication

--- src/main/java/com/ragul/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/com/ragul/springsecuritydemo/config/SecurityConfig.java
@@ -1,21 +1,23 @@
 package com.ragul.springsecuritydemo.config;
 
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static com.ragul.springsecuritydemo.entity.Role.ADMIN;
+import static com.ragul.springsecuritydemo.entity.Role.USER;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final AuthenticationProvider authenticationProvider;
@@ -25,6 +27,7 @@ public class SecurityConfig {
         http
                 .csrf().disable()
                 .authorizeHttpRequests()
+                .requestMatchers("/api/v1/user/all").hasRole(USER.name())
                 .requestMatchers("/api/v1/auth/**")
                 .permitAll()
                 .anyRequest()
